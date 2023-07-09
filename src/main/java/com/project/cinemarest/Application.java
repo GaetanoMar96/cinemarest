cc9302466ffba66005f9675215eebdff59e23e46
Gaetano Maranzino <gaetanomaranzino63@gmail.com>
2023-07-09 11:33:22 +0200

    Initial commit

---
+++ src/main/java/com/project/cinemarest/Application.java
@@ -0,0 +1,13 @@
+package com.project.cinemarest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemarestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemarestApplication.class, args);
+	}
+
+}
